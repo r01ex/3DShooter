d02dd65aa8cc52330e7f7672138ee938459f3054
skku-sleepwalk <lucas7211@g.skku.edu>
2023-10-29 15:53:41 +0900

    music Applied

--- LegendaryCatNayangNayang/Assets/catDefault.cs
+++ LegendaryCatNayangNayang/Assets/catDefault.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Windows;
 
 public class catDefault : MonoBehaviour
 {public RawImage catcat;
     public Texture catcat2;
     private int flag = 0;
+    private int startflag = 0;
+    public AudioSource audioSource;
+    public AudioSource audioSource2;
     IEnumerator changeImg()
     {
         
             
             yield return new WaitForSeconds(2.0f);
         catcat.texture = catcat2;
+        startflag = 1;
+
 
     }
     void Start()
@@ -27,6 +33,16 @@ public class catDefault : MonoBehaviour
         {
      StartCoroutine("changeImg");
             flag++;
+        }
+        if (startflag == 1)
+        {
+            if (UnityEngine.Input.GetMouseButtonDown(0))
+            {
+                audioSource.loop = false;
+                audioSource.Stop();
+                audioSource2.loop = false;
+                audioSource2.Play();
+}
         }
    
     }
